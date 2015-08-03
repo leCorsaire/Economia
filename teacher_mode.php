7c8fb54b62164d7873c48428ea5cb52accd3090e
Sam <samuel.barnades@icloud.com>
2015-08-03 10:05:11 +0200

    Effacement du script et regroupement
    Le script a été regroupé dans un fichier annexe.

---
+++ teacher_mode.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Essai QCM</title>
+		<meta charset="utf-8">
+		<style>
+		
+		
+			.rad_label
+			{
+				display: inline;
+				margin: 10px;
+			}
+			
+			input[type=text]
+			{
+				display: block;
+				margin : 10px;
+				width: 300px;
+				height: 30px; 
+			}
+			
+			input[type=radio], .rad_label
+			{
+				position: relative;
+				left: 330px;
+				bottom: 40px;
+			}
+		</style>
+	</head>
+	<body>
+	
+	<script src="jquery.js"></script>
+	<script src="qcm_script.js"></script>
+	
+	</body>
+	
+	
+</html>
