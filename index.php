e14c72995c71a329b82b7cf65f5abdaaa6c88d54
Sam <samuel.barnades@icloud.com>
2015-08-24 09:56:57 +0200

    màj

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Accueil</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	</head>
+	<body>
+		<form action="teacher_mode.php" method="POST">
+			<p>Identifiant<input type="text" name="id" id="id"></input></p>
+			<p>Mot de passe<input type="password" name="pass" id="pass"></input></p>
+			<input type="submit" label="Connexion"></input>
+		</form>
+	</body>
+</htmL>
--- teacher_mode.php
+++ teacher_mode.php
@@ -5,8 +5,14 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
+	
+	<?php
+		print_r($_POST);
+	?>
+	
 	<div class="container-fluid">
 	<script src="js/jquery.js"></script>
 	<script src="js/noelia.js"></script> <!-- Redirection du script Noelia -->
