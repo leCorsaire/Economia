29c30c20e5ca90e5a3a2bef96804cc32b607836f
Sam <samuel.barnades@icloud.com>
2015-09-03 14:27:04 +0200

    màj
    Mise à jour de l’espace prof.

---
+++ js/creer-cours.js
@@ -0,0 +1,37 @@
+$('#creer-cours').click(function creercours() {
+
+	var c_titre = '<input class="champ form-control" type="text" value="Titre du cours"></input>';
+	var c_s_titre = '<input class="champ form-control" type="text" value="Sous-titre du cours"></input>';
+	var c_desc = '<input class="champ form-control" name="editor1" id="editor1" type="textarea" value="Description du cours"></input>';
+	var c_lien = '<input class="champ form-control" type="text" value="Lien de la vidéo de cours"></input>';
+	var btn_envoyer = '<input class="btn btn-primary" type="submit" value="Valider le cours"></input>';
+	var btn_annuler = '<input class="btn btn-default" type="button" value="Annuler le cours"></input>';
+	
+	$('#creer-cours').hide();
+	
+	$('<form method="POST" action="create_lesson_treatment.php"></form>').appendTo('.container-fluid');
+	
+	$('<div class="row"></div>').appendTo('form');
+		$('<div id="cadre-video" class="col-md-7"></div>').appendTo('.row');
+			$('<a id="video" href="#"></a>').appendTo('#cadre-video');
+				$(c_lien).appendTo('#cadre-video');
+				$('<img class="img-responsive" src="http://placehold.it/700x300" alt="">').appendTo('#video');
+        $('<div id="cadre-info" class="col-md-5"></div>').appendTo('.row');
+			$(c_titre).appendTo('#cadre-info');
+			$(c_s_titre).appendTo('#cadre-info');
+			$(c_desc).appendTo('#cadre-info');
+			$(btn_envoyer).appendTo('#cadre-info');
+			$(btn_annuler).appendTo('#cadre-info').click(function() {
+			
+				$('form').remove();
+				$('#creer-cours').show();
+			
+			});
+			
+                CKEDITOR.replace('editor1');
+
+			
+
+	
+
+});
--- teacher_mode.php
+++ teacher_mode.php
@@ -1,21 +1,55 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Essai QCM</title>
+		<title>Espace professeur</title>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/s_teacher_mode.css">
+		<script src="//cdn.ckeditor.com/4.5.3/basic/ckeditor.js"></script>
 		<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+   		 <!-- Bootstrap Core CSS -->
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+   		 <!-- Custom CSS -->
+   		 <link href="css/1-col-portfolio.css" rel="stylesheet">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
-	<body>
-	
-	<?php
-		print_r($_POST);
-	?>
-	
+	<body>	
 	<div class="container-fluid">
+	 <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img id="logo" src="img/economia_logo.png"></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a id="creer-cours" href="#">Créer un cours</a>
+                    </li>
+                    <li>
+                        <a id="editer-cours" href="#">Editer mes cours</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    
 	<script src="js/jquery.js"></script>
-	<script src="js/noelia.js"></script> <!-- Redirection du script Noelia -->
+	
+	<!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+	<script src="js/creer-cours.js"></script>
 	</div>
 	</body>
 	
